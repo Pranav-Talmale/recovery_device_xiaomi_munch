2b1ecd227627f3ac38ec65fff3715a51d0c7350e
Darth9 <reyprimerjefe@gmail.com>
2025-11-04 23:04:45 +0000

    Work around a (hash verification) ROM flashing error caused by crDroid+NikGapps

--- device.mk
+++ device.mk
@@ -122,3 +122,11 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.virtual_ab.skip_snapshot_creation=true
 
+#--------------------------
+# to work around a (hash verification) ROM flashing error caused by crDroid+NikGapps on alioth
+# (see the edits in update_engine's payload_consumer/filesystem_verifier_action.cc -
+# ro.virtual_ab.skip_verify_source_hash=true)
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.virtual_ab.skip_verify_source_hash=true
+#--------------------------
+#
